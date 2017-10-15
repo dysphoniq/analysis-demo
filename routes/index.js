f19fb4f25b30219cb92f29f841f352f36d7ff947
dysphoniq <crenfrew14@gmail.com>
2017-10-15 14:33:15 -0500

    move cassandra to seperate route

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var cassandra = require('cassandra-driver');
-
-var client = new cassandra.Client({contactPoints: ['localhost']});
-client.connect(function (err, result) {
-  console.log('index: cassandra connected');
-});
-
-var getAllSubscribers = 'SELECT * FROM people.subscribers';
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.execute(getAllSubscribers, [], function (err, result) {
-    if (err) {
-      console.log(err)
-      res.status(404).send({msg: err});
-    } else {
-      console.log(result.rows[0].email);
-      res.render('index',
-          {title: 'Cassandra Test',
-            subscribers: result.rows}
-      );
-    }
-  });
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
