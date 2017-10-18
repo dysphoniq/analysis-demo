275cbd9fbd973bac40a5b7ee7813aa77923c3227
dysphoniq <crenfrew14@gmail.com>
2017-10-17 22:39:54 -0500

    add Cassandra functionality

--- routes/cassandra.js
+++ routes/cassandra.js
@@ -4,26 +4,68 @@ var cassandra = require('cassandra-driver');
 
 var client = new cassandra.Client({contactPoints: ['localhost']});
 client.connect(function (err, result) {
-  console.log('index: cassandra connected');
+  if (err) {
+    console.log(err);
+  }
 });
 
-var getAllSubscribers = 'SELECT * FROM people.subscribers';
+const getAllPersons = 'SELECT * FROM contacts.emails';
+const addPerson = 'INSERT INTO contacts.emails(id, first_name, last_name, email) VALUES (?, ?, ?, ?)';
+const removePerson = 'DELETE FROM contacts.emails WHERE id=?';
+const updatePerson = 'UPDATE contacts.emails SET first_name=?, last_name=?, email=? WHERE id=?';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.execute(getAllSubscribers, [], function (err, result) {
+  client.execute(getAllPersons, [], function (err, result) {
     if (err) {
       console.log(err)
       res.status(404).send({msg: err});
     } else {
-      console.log(result.rows[0].email);
       res.render('Cassandra',
           {title: 'Cassandra Test',
-            subscribers: result.rows}
+            entries: result.rows}
       );
     }
   });
-  // res.render('index', { title: 'Express' });
 });
+
+router.post('/insert', function(req, res, next) {
+  var values = [req.body.id, req.body.first_name, req.body.last_name, req.body.email];
+  client.execute(addPerson, values, {prepare : true}, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send({msg: err});
+    } else {
+      res.status(200).send('success');
+    }
+  });
+});
+
+router.post('/update', function (req, res, next) {
+  var values = [req.body.first_name, req.body.last_name, req.body.email, req.body.id];
+  client.execute(updatePerson, values, {prepare : true}, function (err, result) {
+    if (err) {
+      console.log(err);
+      res.status(404).send({msg: err});
+    } else {
+      res.status(200).send('success');
+    }
+  });
+
+});
+
+router.post('/delete', function (req, res, next) {
+  var value = [req.body.id];
+  console.log(value[0]);
+  client.execute(removePerson, value, {prepare : true}, function (err, results) {
+    if (err) {
+      console.log(err);
+      res.status(404).send({msg: err});
+    } else {
+      res.status(200).send('success');
+    }
+  });
+});
+
 
 module.exports = router;
